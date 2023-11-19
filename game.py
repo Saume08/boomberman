df739d390d73d86279a5cf66416ae3b61be4385d
Saume08 <pastidefaso@gmail.com>
2023-11-19 18:46:16 -0300

    deleted:    Arreglos.txt 	new file:   Cambios para hacer.txt 	modified:   game.py

--- game.py
+++ game.py
@@ -206,8 +206,8 @@ def main(s, tile_size, show_path, terrain_images, bomb_images, explosion_images,
         #controla la velocidad del juegooo estaba en 15 como default
         dt = clock.tick(14)
         ####intento de pausa, yo me puedo mover, pero el resto del juego se paraliza, pasa algo raro si spameo la p
-        if keys[pygame.K_p]:
-            dt = clock.tick(0)
+       # if keys[pygame.K_p]:
+        #    dt = clock.tick(0)
             #######################################
         for en in enemy_list:
             en.make_move(grid, bombs, explosions, ene_blocks)
